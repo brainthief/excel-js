efe6e49ccf670d96c09360b992229588dc34a622
Vytautas <vytautas1983@gmail.com>
2020-10-04 22:40:48 +0300

    project config (#1)

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+ parser: 'babel-eslint',
+ env: {
+  es6: true,
+  browser: true,
+  node: true,
+ },
+ rules: {
+  semi: 'off',
+  indent: ['error', 1],
+  'linebreak-style': 'off',
+  'quote-props': 'off',
+  'object-curly-spacing': 'off',
+ },
+ extends: ['eslint:recommended', 'google'],
+}
